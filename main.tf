f1ca7d7ff8025093f163bfa33b43ca70f5f658a4
wallacepf <wallacepf@me.com>
2021-06-22 17:44:26 -0300

    Env vars

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 provider "aws" {
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
+  access_key = var.aws_access_key_id
+  secret_key = var.aws_secret_access_key
   region     = var.aws_region
 }
 
