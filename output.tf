132d1e456792b62f58f2ac09cdf85196d757561d
wallacepf <wallacepf@me.com>
2021-06-22 18:13:44 -0300

    Changed Outputs

--- output.tf
+++ output.tf
@@ -18,6 +18,18 @@ output "production_subnet_id" {
   value = aws_subnet.production.id
 }
 
+output "development_subnet" {
+  value = aws_subnet.development.cidr_block
+}
+
+output "staging_subnet" {
+  value = aws_subnet.staging.cidr_block
+}
+
+output "production_subnet" {
+  value = aws_subnet.production.cidr_block
+}
+
 output "region" {
   value = var.aws_region
 }
