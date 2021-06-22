01abef3f645c63aaf1567c45c0aa4b07ac33b4d2
wallacepf <wallacepf@me.com>
2021-06-22 17:25:09 -0300

    First Commit

---
+++ main.tf
@@ -0,0 +1,36 @@
+provider "aws" {
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+  region     = var.aws_region
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = var.cidr_block
+}
+
+resource "aws_subnet" "development" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.subnet_dev
+
+  tags = {
+    Name = "development"
+  }
+}
+
+resource "aws_subnet" "staging" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.subnet_staging
+
+  tags = {
+    Name = "staging"
+  }
+}
+
+resource "aws_subnet" "production" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.subnet_prod
+
+  tags = {
+    Name = "production"
+  }
+}
---
+++ output.tf
@@ -0,0 +1,23 @@
+output "main_vpc" {
+  value = aws_vpc.main.id
+}
+
+output "default_security_group_id" {
+  value = aws_vpc.main.default_security_group_id
+}
+
+output "development_subnet_id" {
+  value = aws_subnet.development.id
+}
+
+output "staging_subnet_id" {
+  value = aws_subnet.staging.id
+}
+
+output "production_subnet_id" {
+  value = aws_subnet.production.id
+}
+
+output "region" {
+  value = var.aws_region
+}
