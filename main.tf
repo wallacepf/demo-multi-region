97005ac42d66f7febbaf6679bca1aedc749051e3
wallacepf <wallacepf@gmail.com>
2021-07-08 13:08:30 -0300

    Fixes

--- main.tf
+++ main.tf
@@ -59,7 +59,7 @@ module "demo-ec2-module" {
   inst_name = var.inst_name
   inst_size = var.inst_size
   inst_key_name = var.inst_name
-  inst_sec_group_id = aws_security_group.inst_default_rules
+  inst_sec_group_id = aws_security_group.inst_default_rules.id
   inst_subnet_id = aws_subnet.development.id
   inst_common_tags = var.inst_common_tags
 }
