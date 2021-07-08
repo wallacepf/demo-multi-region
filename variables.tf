e9eab38b474c2bb31b898af98442afdc9a05004a
wallacepf <wallacepf@gmail.com>
2021-07-08 11:58:54 -0300

    Additional Fixes

--- variables.tf
+++ variables.tf
@@ -3,3 +3,24 @@ variable "cidr_block" {}
 variable "subnet_dev" {}
 variable "subnet_staging" {}
 variable "subnet_prod" {}
+variable "inst_size" {}
+variable "inst_key_name" {}
+variable "inst_sec_group_id" {}
+variable "inst_subnet_id" {}
+variable "inst_name" {}
+
+variable "inst_ami" {
+    default = "ami-03d5c68bab01f3496"
+}
+
+variable "inst_count" {
+  default = "1"
+}
+
+variable "inst_common_tags" {
+  type = map(string)
+  default = {
+    "Description" : "Computing Tier",
+    "Owner" : "Platform Team"
+  }
+}
