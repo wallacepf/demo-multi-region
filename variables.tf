952cd188ae2eebddbcb8de6f89c85c88df37ae7c
wallacepf <wallacepf@gmail.com>
2021-09-03 10:09:11 -0300

    Teste Commit

--- variables.tf
+++ variables.tf
@@ -1,7 +1,4 @@
 variable "aws_region" {}
-variable "teste" {
-  default="teste123456"
-}
 variable "cidr_block" {}
 variable "subnet_dev" {}
 variable "subnet_staging" {}
@@ -11,7 +8,7 @@ variable "inst_key_name" {}
 variable "inst_name" {}
 
 variable "inst_count" {
-  default = "1"
+  default = "2"
 }
 
 variable "inst_common_tags" {
